ddc41a6258321a3d83a0e3f5d6bee9c3f8e22199
Faili <321Faily@gmail.com>
2024-04-08 13:59:42 +0200

    [TASK] add initial site package and adjust site config (#1)

---
+++ local_packages/portfolio/ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'TYPO3 Portfolio Set',
+    'description' => '',
+    'category' => '',
+    'state' => 'alpha',
+    'author' => 'Felix Pachowsky',
+    'author_email' => 'f.pachowsky@googlemail.com',
+    'author_company' => '',
+    'version' => '0.9',
+    'constraints' => [
+        'depends' => [
+            'php' => '8.2-',
+            'typo3' => '11.5-',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
