4f9f6097b516c90bb13584423436796ef70b6316
Lisa-Maria Schedlberger <ls@studiomitte.com>
2024-04-10 17:59:48 +0200

    [FEATURE] add clients logo CE

--- local_packages/portfolio/ext_tables.sql
+++ local_packages/portfolio/ext_tables.sql
@@ -83,3 +83,26 @@ CREATE TABLE tx_portfolio_service (
     KEY              parent (pid),
     KEY language (l10n_parent,sys_language_uid)
 );
+
+CREATE TABLE tx_portfolio_client_logo (
+    uid              int(11) NOT NULL auto_increment,
+    pid              int(11) DEFAULT '0' NOT NULL,
+    tstamp           int(11) DEFAULT '0' NOT NULL,
+    crdate           int(11) DEFAULT '0' NOT NULL,
+    cruser_id        int(11) DEFAULT '0' NOT NULL,
+    sys_language_uid int(11) DEFAULT '0' NOT NULL,
+    l10n_parent      int(11) DEFAULT '0' NOT NULL,
+    l10n_diffsource  mediumtext,
+    l10n_source      int(11) DEFAULT '0' NOT NULL,
+    sorting          int(10) DEFAULT '0' NOT NULL,
+    deleted          tinyint(4) DEFAULT '0' NOT NULL,
+    hidden           tinyint(4) DEFAULT '0' NOT NULL,
+    parent           int(11) DEFAULT '0' NOT NULL,
+
+    name varchar(255) DEFAULT ''  NOT NULL,
+    image INT(11) UNSIGNED DEFAULT '0' NOT NULL,
+
+    PRIMARY KEY (uid),
+    KEY              parent (pid),
+    KEY language (l10n_parent,sys_language_uid)
+);
