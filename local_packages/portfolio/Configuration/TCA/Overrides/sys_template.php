6137c034e56a5655bd118cee945cb1c0af7aae64
Benjamin Kott <benjamin.kott@outlook.com>
2024-04-10 15:40:22 +0100

    [TASK] Cleanup

--- local_packages/portfolio/Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'portfolio',
-    'Configuration/TypoScript',
-    'Portfolio Hack'
-);
--- local_packages/portfolio/ext_localconf.php
+++ local_packages/portfolio/ext_localconf.php
@@ -5,28 +5,24 @@ $boot = static function (): void {
 // Register custom EXT:form configuration
     if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('form')) {
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
-            trim(
-        '
-                    module.tx_form {
-                        settings {
-                            yamlConfigurations {
-                                110 = EXT:portfolio/Configuration/Form/Setup.yaml
-                            }
+            trim('
+                module.tx_form {
+                    settings {
+                        yamlConfigurations {
+                            110 = EXT:portfolio/Configuration/Form/Setup.yaml
                         }
                     }
-                    plugin.tx_form {
-                        settings {
-                            yamlConfigurations {
-                                110 = EXT:portfolio/Configuration/Form/Setup.yaml
-                            }
+                }
+                plugin.tx_form {
+                    settings {
+                        yamlConfigurations {
+                            110 = EXT:portfolio/Configuration/Form/Setup.yaml
                         }
                     }
-                '
-            )
+                }
+            ')
         );
     }
-
-    $GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'][] = 'portfolio/Configuration/TypoScript/';
 };
 
 $boot();
