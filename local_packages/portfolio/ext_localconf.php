674fa473ceba2f92c02b4f6773d30b35d2817b9d
Faili <321Faily@gmail.com>
2024-04-10 16:29:49 +0200

    [FEATURE] add contact form

--- local_packages/portfolio/ext_localconf.php
+++ local_packages/portfolio/ext_localconf.php
@@ -1,2 +1,33 @@
 <?php
 declare(strict_types=1);
+
+$boot = static function (): void {
+// Register custom EXT:form configuration
+    if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('form')) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+            trim(
+        '
+                    module.tx_form {
+                        settings {
+                            yamlConfigurations {
+                                110 = EXT:portfolio/Configuration/Form/Setup.yaml
+                            }
+                        }
+                    }
+                    plugin.tx_form {
+                        settings {
+                            yamlConfigurations {
+                                110 = EXT:portfolio/Configuration/Form/Setup.yaml
+                            }
+                        }
+                    }
+                '
+            )
+        );
+    }
+
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'][] = 'portfolio/Configuration/TypoScript/';
+};
+
+$boot();
+unset($boot);
