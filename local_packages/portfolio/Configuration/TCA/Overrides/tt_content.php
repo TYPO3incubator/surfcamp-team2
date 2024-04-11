ab5b11cd6f0a2c656309b468ce9074cb2b177c4a
Benjamin Kott <benjamin.kott@outlook.com>
2024-04-11 14:59:40 +0100

    [TASK] Cleanup plugin registration

--- local_packages/portfolio/Configuration/TCA/Overrides/tt_content.php
+++ local_packages/portfolio/Configuration/TCA/Overrides/tt_content.php
@@ -1,45 +1,44 @@
 <?php
-declare(strict_types=1);
 
-$boot = static function (): void {
-    $LLL = 'LLL:EXT:portfolio/Resources/Private/Language/locallang_db.xlf:';
+defined('TYPO3') or die();
 
-    $GLOBALS['TCA']['tt_content']['columns']['link'] = [
-        'label' => $LLL . 'field.link',
-        'config' => [
-            'type' => 'input',
-            'renderType' => 'inputLink',
-            'size' => 50,
-            'max' => 1024,
-            'eval' => 'trim',
-            'softref' => 'typolink',
-            'fieldControl' => [
-                'linkPopup' => [
-                    'options' => [
-                        'title' => $LLL . 'field.link.formlabel',
-                        'windowOpenParameters' => 'height=800,width=1000,status=0,menubar=0,scrollbars=1',
-                    ],
+$GLOBALS['TCA']['tt_content']['columns']['link'] = [
+    'label' => 'LLL:EXT:portfolio/Resources/Private/Language/locallang_db.xlf:field.link',
+    'config' => [
+        'type' => 'input',
+        'renderType' => 'inputLink',
+        'size' => 50,
+        'max' => 1024,
+        'eval' => 'trim',
+        'softref' => 'typolink',
+        'fieldControl' => [
+            'linkPopup' => [
+                'options' => [
+                    'title' => 'LLL:EXT:portfolio/Resources/Private/Language/locallang_db.xlf:field.link.formlabel',
+                    'windowOpenParameters' => 'height=800,width=1000,status=0,menubar=0,scrollbars=1',
                 ],
             ],
         ],
-    ];
+    ],
+];
 
-    $GLOBALS['TCA']['tt_content']['columns']['link_text'] = [
-        'label' => $LLL . 'field.link_text',
-        'config' => [
-            'type' => 'input',
-            'size' => 50,
-            'max' => 255,
-            'eval' => 'trim',
-        ],
-    ];
-};
+$GLOBALS['TCA']['tt_content']['columns']['link_text'] = [
+    'label' => 'LLL:EXT:portfolio/Resources/Private/Language/locallang_db.xlf:field.link_text',
+    'config' => [
+        'type' => 'input',
+        'size' => 50,
+        'max' => 255,
+        'eval' => 'trim',
+    ],
+];
 
-$boot();
-unset($boot);
+call_user_func(static function () {
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'Portfolio',
-    'Project',
-    'Projects',
-);
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+        'Portfolio',
+        'Projects',
+        'LLL:EXT:portfolio/Resources/Private/Language/locallang_db.xlf:content_element.portfolio_project',
+        'icon_project'
+    );
+
+});
