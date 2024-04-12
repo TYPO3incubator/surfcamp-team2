0f97d850f4b72a5b85e2100a7b54f4259672d47b
Benjamin Kott <benjamin.kott@outlook.com>
2024-04-12 17:49:43 +0100

    [TASK] Add client logos (#95)
    * [TASK] Prepare frontend for client logo element
    
    * [TASK] Add svg inline view helper
    
    * [TASK] Style and change client logos to svg
    
    ---------
    
    Co-authored-by: David Bonhagen <david.bonhagen@fullhaus.de>

---
+++ local_packages/portfolio/Classes/ViewHelpers/InlineSvgViewHelper.php
@@ -0,0 +1,67 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the package bk2k/bootstrap-package.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace FriendsOfTYPO3\ThemePortfolio\ViewHelpers;
+
+use Closure;
+use FriendsOfTYPO3\ThemePortfolio\Utility\SvgUtility;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Exception;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
+
+/**
+ * InlineSvgViewHelper
+ */
+class InlineSvgViewHelper extends AbstractViewHelper
+{
+    use CompileWithRenderStatic;
+
+    /**
+     * @var bool
+     */
+    protected $escapeOutput = false;
+
+    /**
+     * Initialize arguments.
+     *
+     * @throws Exception
+     * @return void
+     */
+    public function initializeArguments(): void
+    {
+        parent::initializeArguments();
+        $this->registerArgument('image', 'object', 'a FAL object');
+        $this->registerArgument('src', 'string', 'a path to a file');
+        $this->registerArgument('class', 'string', 'Css class for the svg');
+        $this->registerArgument('width', 'string', 'Width of the svg.');
+        $this->registerArgument('height', 'string', 'Height of the svg.');
+    }
+
+    /**
+     * @param array $arguments
+     * @param Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return string
+     * @throws \Exception
+     */
+    public static function renderStatic(
+        array $arguments,
+        Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    ): string {
+        $src = (string)$arguments['src'];
+        $image = $arguments['image'];
+        $width = $arguments['width'] ? (int) $arguments['width'] : null;
+        $height = $arguments['height'] ? (int) $arguments['height'] : null;
+        $class = $arguments['class'] ? (string) $arguments['class'] : null;
+
+        return SvgUtility::getInlineSvg($src, $image, $width, $height, $class);
+    }
+}
