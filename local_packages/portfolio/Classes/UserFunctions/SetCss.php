83ca9b29f6faec124b0ce4fbb8cf3e0929b2c600
Vanessa M <vay.marzi@googlemail.com>
2024-04-12 13:56:53 +0200

    [TASK] Theming (#72)
    * Add base theming for colors and border radius
    
    * Add variable fonts option
    
    * [TASK] Remove files and styles of fonts and primary / secondary color provided by the frontend
    
    * Add sets center option
    
    * Add base theming for colors and border radius
    
    * Add variable fonts option
    
    * [TASK] Remove files and styles of fonts and primary / secondary color provided by the frontend
    
    * Add sets center option
    
    * Add Roboto webfont
    
    * [FEATURE] add option for configurable font
    
    * [BUGFIX] fix coloring via sets
    
    * [BUGFIX] fix coloring via sets
    
    * [FEATURE] add content alignment as set setting
    
    * Apply content alignment to introduction card, services and resume ce
    
    * [BUGFIX] set site correct
    
    * [TASK] add override for forms
    
    ---------
    
    Co-authored-by: Vanessa Marcinczak <vanessa@neues.studio>
    Co-authored-by: Faili <321Faily@gmail.com>
    Co-authored-by: Benjamin Kott <benjamin.kott@outlook.com>

--- local_packages/portfolio/Classes/UserFunctions/SetCss.php
+++ local_packages/portfolio/Classes/UserFunctions/SetCss.php
@@ -5,9 +5,75 @@ declare(strict_types=1);
 namespace Surfcamp\Portfolio\UserFunctions;
 
 use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Utility\PathUtility;
+
+use function implode;
+use function str_replace;
 
 final class SetCss
 {
+    private const FONT_MARKUP = <<<END
+        :root {
+            --font-family-heading:  '#FONT_NAME#', 'Georgia', serif;
+            --font-family-copytext: '#COPY_FONT_NAME#', 'Tahoma', sans-serif;
+        }
+        
+        // Headline Font in Bold
+        @font-face {
+            font-family: '#FONT_NAME#';
+            src: url(".#PATH##FONT_NAME_TRIMMED#-Bold.woff2") format("woff2");
+            font-weight: 700;
+            font-style: normal;
+            font-display: swap;
+        }
+        
+        // Headline Font in Medium
+        @font-face {
+            font-family: '#FONT_NAME#';
+            src: url("#PATH##FONT_NAME_TRIMMED#-Medium.woff2") format("woff2");
+            font-weight: 500;
+            font-style: normal;
+            font-display: swap;
+        }
+        
+        // Headline Font in Regular
+        @font-face {
+            font-family: '#FONT_NAME#';
+            src: url("#PATH##FONT_NAME_TRIMMED#-Regular.woff2") format("woff2");
+            font-weight: 400;
+            font-style: normal;
+            font-display: swap;
+        }
+        
+        // Copytext Font in Bold
+        @font-face {
+            font-family: '#COPY_FONT_NAME#';
+            src: url("#PATH##COPY_FONT_NAME_TRIMMED#-Bold.woff2") format("woff2");
+            font-weight: 700;
+            font-style: normal;
+            font-display: swap;
+        }
+        
+        // Copytext Font in Medium
+        @font-face {
+            font-family: '#COPY_FONT_NAME#';
+            src: url("#PATH##COPY_FONT_NAME_TRIMMED#-Medium.woff2") format("woff2");
+            font-weight: 500;
+            font-style: normal;
+            font-display: swap;
+        }
+        
+        // Copytext Font in Regular
+        @font-face {
+            font-family: '#COPY_FONT_NAME#';
+            src: url("#PATH##COPY_FONT_NAME_TRIMMED#-Regular.woff2") format("woff2");
+            font-weight: 400;
+            font-style: normal;
+            font-display: swap;
+        }
+    END;
+
+
     public function getBodyDataModifier(string $content, array $conf, ServerRequestInterface $request): string
     {
         $settings = $this->getSettings($request);
@@ -24,14 +90,33 @@ final class SetCss
         $settings = $this->getSettings($request);
         $styleVariables = [];
         foreach ($settings['style']['variables'] as $item) {
-            $styleVariables[] = '--' . $item['key'] . ': ' . $item['value'];
+            $styleVariables[] = '--' . $item['key'] . ': ' . $item['value'] . '; ';
         }
 
-        return '<style>' . implode(';', $styleVariables) . '</style>';
+        $font = $this->getCustomFont($settings);
+
+        return '<style>' .
+            ':root {' .
+            implode(PHP_EOL, $styleVariables) . '} ' . $font . '
+</style>';
     }
 
     protected function getSettings(ServerRequestInterface $request): array
     {
         return $request->getAttribute('site')->getConfiguration()['settings'];
+    }
+
+    private function getCustomFont(array $settings): string
+    {
+        $font = $settings['style']['font'];
+        $copyFont = $settings['style']['copyFont'];
+
+        $path = PathUtility::getPublicResourceWebPath('EXT:portfolio/Resources/Public/Fonts/');
+
+        return str_replace(
+            ['#FONT_NAME#', '#COPY_FONT_NAME#', '#FONT_NAME_TRIMMED#', '#COPY_FONT_NAME_TRIMMED#', '#PATH#'],
+            [$font, $copyFont, str_replace(' ', '', $font), str_replace(' ', '', $copyFont), $path],
+            self::FONT_MARKUP
+        );
     }
 }
