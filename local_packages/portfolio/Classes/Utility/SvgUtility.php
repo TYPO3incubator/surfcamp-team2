32be8907327704c80836c4f1c09c9ea814b6960f
Benjamin Kott <benjamin.kott@outlook.com>
2024-04-12 18:53:51 +0200

    [TASK] Add missing compiled files and minor cleanup

--- local_packages/portfolio/Classes/Utility/SvgUtility.php
+++ local_packages/portfolio/Classes/Utility/SvgUtility.php
@@ -1,13 +1,6 @@
 <?php
 declare(strict_types = 1);
 
-/*
- * This file is part of the package bk2k/bootstrap-package.
- *
- * For the full copyright and license information, please read the
- * LICENSE file that was distributed with this source code.
- */
-
 namespace FriendsOfTYPO3\ThemePortfolio\Utility;
 
 use TYPO3\CMS\Core\Resource\Exception\ResourceDoesNotExistException;
--- local_packages/portfolio/Classes/ViewHelpers/InlineSvgViewHelper.php
+++ local_packages/portfolio/Classes/ViewHelpers/InlineSvgViewHelper.php
@@ -1,12 +1,5 @@
 <?php declare(strict_types=1);
 
-/*
- * This file is part of the package bk2k/bootstrap-package.
- *
- * For the full copyright and license information, please read the
- * LICENSE file that was distributed with this source code.
- */
-
 namespace FriendsOfTYPO3\ThemePortfolio\ViewHelpers;
 
 use Closure;
